c38bd0ed8560808db29f50bc382fb808e98c26f0
domain2 <domain2@nust.edu.pk>
2024-11-30 03:19:27 -0800

    new

--- ps3/test/expressivo/AdditionExpressionTest.java
+++ ps3/test/expressivo/AdditionExpressionTest.java
@@ -16,10 +16,89 @@ public class AdditionExpressionTest {
         assertEquals("10.0000+20.0000", addExpr1.toString());
         
         // Test equals
-        assertFalse(addExpr1.equals(addExpr2));
-        assertTrue(addExpr1.equals(new AdditionExpression(exp1, exp2)));
+        assertFalse(addExpr1.equals(addExpr2));  // Different operand order
+        assertTrue(addExpr1.equals(new AdditionExpression(exp1, exp2)));  // Same operands
         
         // Test hashCode
-        assertNotEquals(addExpr1.hashCode(), addExpr2.hashCode());
+        assertNotEquals(addExpr1.hashCode(), addExpr2.hashCode());  // Different operand order
+    }
+
+    // Test with negative numbers
+    @Test
+    public void testAdditionWithNegativeNumbers() {
+        Expression exp1 = new NumberExpression(-10);
+        Expression exp2 = new NumberExpression(20);
+        Expression addExpr = new AdditionExpression(exp1, exp2);
+
+        // Test toString with negative number
+        assertEquals("-10.0000+20.0000", addExpr.toString());
+        
+        // Test equals
+        Expression exp3 = new NumberExpression(10);
+        Expression exp4 = new NumberExpression(30);
+        Expression addExpr2 = new AdditionExpression(exp3, exp4);
+        assertFalse(addExpr.equals(addExpr2));  // Different operands
+
+        // Test hashCode
+        assertNotEquals(addExpr.hashCode(), addExpr2.hashCode());
+    }
+
+    // Test with floating-point numbers
+    @Test
+    public void testAdditionWithFloatingPointNumbers() {
+        Expression exp1 = new NumberExpression(10.5);
+        Expression exp2 = new NumberExpression(20.3);
+        Expression addExpr = new AdditionExpression(exp1, exp2);
+
+        // Test toString with floating-point numbers
+        assertEquals("10.5000+20.3000", addExpr.toString());
+        
+        // Test equality
+        Expression exp3 = new NumberExpression(15.0);
+        Expression exp4 = new NumberExpression(15.8);
+        Expression addExpr2 = new AdditionExpression(exp3, exp4);
+        assertFalse(addExpr.equals(addExpr2));  // Different operands
+
+        // Test hashCode
+        assertNotEquals(addExpr.hashCode(), addExpr2.hashCode());
+    }
+
+
+    // Test adding same operand twice (e.g., x + x)
+    @Test
+    public void testAdditionWithSameOperands() {
+        Expression exp1 = new NumberExpression(10);
+        Expression addExpr = new AdditionExpression(exp1, exp1);
+
+        // Test toString for same operands
+        assertEquals("10.0000+10.0000", addExpr.toString());
+        
+        // Test equality with same operands in different order
+        Expression exp2 = new NumberExpression(10);
+        Expression addExpr2 = new AdditionExpression(exp2, exp2);
+        assertTrue(addExpr.equals(addExpr2));  // Same operands
+        
+        // Test hashCode
+        assertEquals(addExpr.hashCode(), addExpr2.hashCode());
+    }
+
+    // Test addition with large numbers
+    @Test
+    public void testAdditionWithLargeNumbers() {
+        Expression exp1 = new NumberExpression(1_000_000);
+        Expression exp2 = new NumberExpression(2_000_000);
+        Expression addExpr = new AdditionExpression(exp1, exp2);
+
+        // Test toString for large numbers
+        assertEquals("1000000.0000+2000000.0000", addExpr.toString());
+        
+        // Test equality with the same numbers
+        Expression exp3 = new NumberExpression(1_000_000);
+        Expression exp4 = new NumberExpression(2_000_000);
+        Expression addExpr2 = new AdditionExpression(exp3, exp4);
+        assertTrue(addExpr.equals(addExpr2));
+        
+        // Test hashCode
+        assertEquals(addExpr.hashCode(), addExpr2.hashCode());
     }
 }
--- ps3/test/expressivo/VariableExpressionTest.java
+++ ps3/test/expressivo/VariableExpressionTest.java
@@ -16,11 +16,70 @@ public class VariableExpressionTest {
         assertEquals("x", exp1.toString());
         
         // Test equals
-        assertTrue(exp1.equals(exp3));
-        assertFalse(exp1.equals(exp2));
+        assertTrue(exp1.equals(exp3));  // Same variable name
+        assertFalse(exp1.equals(exp2)); // Different variable names
         
         // Test hashCode
-        assertEquals(exp1.hashCode(), exp3.hashCode());
+        assertEquals(exp1.hashCode(), exp3.hashCode()); // Same variable name
+        assertNotEquals(exp1.hashCode(), exp2.hashCode()); // Different variable names
+    }
+
+    // Test with different variable names
+    @Test
+    public void testDifferentVariableNames() {
+        Expression exp1 = new VariableExpression("a");
+        Expression exp2 = new VariableExpression("b");
+
+        // Different variable names should not be equal
+        assertFalse(exp1.equals(exp2));
+
+        // Different variable names should have different hash codes
         assertNotEquals(exp1.hashCode(), exp2.hashCode());
+    }
+
+    // Test with variable name that includes special characters
+    @Test
+    public void testVariableWithSpecialCharacters() {
+        Expression exp1 = new VariableExpression("var_1");
+        Expression exp2 = new VariableExpression("var_1");
+
+        // Special character in variable name should still be considered in equality
+        assertTrue(exp1.equals(exp2));
+        assertEquals(exp1.hashCode(), exp2.hashCode());
+    }
+
+
+    // Test with longer variable names
+    @Test
+    public void testLongVariableName() {
+        String longVarName = "this_is_a_very_long_variable_name";
+        Expression exp1 = new VariableExpression(longVarName);
+        Expression exp2 = new VariableExpression(longVarName);
+
+        // Long variable names should still be considered equal
+        assertTrue(exp1.equals(exp2));
+        assertEquals(exp1.hashCode(), exp2.hashCode());
+    }
+
+    // Test with variables with the same name but created in different instances
+    @Test
+    public void testSameNameDifferentInstances() {
+        Expression exp1 = new VariableExpression("temp");
+        Expression exp2 = new VariableExpression("temp");
+
+        // Same variable name in different instances should be equal
+        assertTrue(exp1.equals(exp2));
+        assertEquals(exp1.hashCode(), exp2.hashCode());
+    }
+
+    // Test toString on different variable names
+    @Test
+    public void testToStringWithDifferentNames() {
+        Expression exp1 = new VariableExpression("foo");
+        Expression exp2 = new VariableExpression("bar");
+
+        // Check that toString returns correct string representation
+        assertEquals("foo", exp1.toString());
+        assertEquals("bar", exp2.toString());
     }
 }
